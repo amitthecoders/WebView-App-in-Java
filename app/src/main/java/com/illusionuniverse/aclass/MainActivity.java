cf084b2377d66a0ade804e9c543c3ccfb5e19048
Amit Kumar <104451914+amitthecoder@users.noreply.github.com>
2022-08-12 15:46:55 +0530

    Initial commit

---
+++ app/src/main/java/com/illusionuniverse/aclass/MainActivity.java
@@ -0,0 +1,34 @@
+package com.illusionuniverse.aclass;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.web);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.illusionuniverse.in");
+        webView.getSettings().setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+
+    }
+}
